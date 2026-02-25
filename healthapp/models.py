fccd4921fe8b0d225eb79b75d68a3cfedae5b02a
Clementvexegon-7 <clementvexegon@gmail.com>
2026-02-25 12:25:52 +0300

    initial commit

--- healthapp/models.py
+++ healthapp/models.py
@@ -20,7 +20,7 @@ class Myappointment(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone =models.CharField(max_length=20)
-    datetime = models.DateField()
+    datetime = models.DateTimeField()
     department = models.CharField(max_length=100)
     doctor = models.CharField(max_length=100)
     message = models.TextField()
